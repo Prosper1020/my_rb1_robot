7e22c7d3eecf0a578437b82dc38162733eb6a531
Koffi Koumi <sigkpe@gmail.com>
2026-03-12 10:47:51 +0000

    Task 1 Fix: Add RViz config file, update display.launch.py to auto-load robot model

--- my_rb1_description/launch/display.launch.py
+++ my_rb1_description/launch/display.launch.py
@@ -6,18 +6,13 @@ from launch_ros.actions import Node
 def generate_launch_description():
     pkg_dir = get_package_share_directory('my_rb1_description')
     urdf_file = os.path.join(pkg_dir, 'urdf', 'my_rb1_robot.urdf')
-    
+    rviz_config = os.path.join(pkg_dir, 'rviz', 'display.rviz')
     with open(urdf_file, 'r') as f:
         robot_description = f.read()
-    
     return LaunchDescription([
-        Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            parameters=[{'robot_description': robot_description}]
-        ),
-        Node(
-            package='joint_state_publisher_gui',
-            executable='joint_state_publisher_gui'
-        ),
+        Node(package='robot_state_publisher', executable='robot_state_publisher',
+             parameters=[{'robot_description': robot_description}]),
+        Node(package='joint_state_publisher_gui', executable='joint_state_publisher_gui'),
+        Node(package='rviz2', executable='rviz2',
+             arguments=['-d', rviz_config]),
     ])
--- my_rb1_description/setup.py
+++ my_rb1_description/setup.py
@@ -14,20 +14,15 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf')),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='user',
-    maintainer_email='sigkpe@gmail.com',
+    maintainer_email='user@todo.todo',
     description='TODO: Package description',
     license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
     entry_points={
-        'console_scripts': [
-        ],
+        'console_scripts': [],
     },
 )
